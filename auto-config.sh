0a83610efd8eabaebef6bb86c1f991fb41ffe03a
shiba_yu36 <shiba1029196473@gmail.com>
2010-07-31 17:59:10 +0900

    configのリンクを自動で貼るscript作成

---
+++ auto-config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -s ~/development/config-file/tscreenrc ~/.tscreenrc
+ln -s ~/development/config-file/zprofile  ~/.zprofile
+ln -s ~/development/config-file/zshenv ~/.zshenv
+ln -s ~/development/config-file/zshrc ~/.zshrc
