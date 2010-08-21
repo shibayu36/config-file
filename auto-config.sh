7a6fe126eee48f4b66fdc9fa34526706f061fca6
shiba_yu36 <shiba1029196473@gmail.com>
2010-08-21 22:48:35 +0900

    gitignore, my.cnf(homeディレクトリ内の), provercを追加

--- auto-config.sh
+++ auto-config.sh
@@ -3,3 +3,6 @@ ln -s ~/development/config-file/tscreenrc ~/.tscreenrc
 ln -s ~/development/config-file/zprofile  ~/.zprofile
 ln -s ~/development/config-file/zshenv ~/.zshenv
 ln -s ~/development/config-file/zshrc ~/.zshrc
+ln -s ~/development/config-file/gitignore ~/.gitignore
+ln -s ~/development/config-file/proverc ~/.proverc
+ln -s ~/development/config-file/my.cnf ~/.my.cnf
