c6e026678411fa3a4c8748b0ad7ea44555f0753e
shiba_yu36 <shibayu36@gmail.com>
2011-03-16 16:58:40 +0900

    .付けた

--- auto-config.sh
+++ auto-config.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-ln -s ~/development/config-file/tscreenrc ~/.tscreenrc
-ln -s ~/development/config-file/zprofile  ~/.zprofile
-ln -s ~/development/config-file/zshenv ~/.zshenv
-ln -s ~/development/config-file/zshrc ~/.zshrc
-ln -s ~/development/config-file/gitignore ~/.gitignore
-ln -s ~/development/config-file/proverc ~/.proverc
-ln -s ~/development/config-file/my.cnf ~/.my.cnf
-ln -s ~/development/config-file/zsh ~/.zsh
-ln -s ~/development/config-file/globalrc ~/.globalrc
-ln -s ~/development/config-file/minicpanrc ~/.minicpanrc
+ln -s ~/development/config-file/.tscreenrc ~/.tscreenrc
+ln -s ~/development/config-file/.zprofile  ~/.zprofile
+ln -s ~/development/config-file/.zshenv ~/.zshenv
+ln -s ~/development/config-file/.zshrc ~/.zshrc
+ln -s ~/development/config-file/.gitignore ~/.gitignore
+ln -s ~/development/config-file/.proverc ~/.proverc
+ln -s ~/development/config-file/.my.cnf ~/.my.cnf
+ln -s ~/development/config-file/.zsh ~/.zsh
+ln -s ~/development/config-file/.globalrc ~/.globalrc
+ln -s ~/development/config-file/.minicpanrc ~/.minicpanrc
 
 #zsh関数群
-ln -s ~/development/config-file/zshrc.function ~/.zshrc.function
+ln -s ~/development/config-file/.zshrc.function ~/.zshrc.function
 
 #bin
 ln -s ~/development/config-file/bin/* ~/bin/
