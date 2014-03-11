d361153a6ff2777c3c028222bd9ba59eb82142f1
shibayu36 <shibayu36@gmail.com>
2014-03-11 21:53:23 +0900

    Fotolife uploader

---
+++ bin/fotolife_gyazo.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+use Config::Pit;
+use IPC::Run qw(run);
+use WebService::Hatena::Fotolife;
+use Path::Tiny;
+
+my $config = pit_get("f.hatena.ne.jp", require => {
+    username   => "username",
+    upload_key => "upload_key",
+    folder     => "folder",
+});
+my $username   = $config->{username};
+my $upload_key = $config->{upload_key};
+my $folder     = $config->{folder};
+
+my $file = path('/tmp/screencapture.png');
+$file->remove;
+system "screencapture -i $file";
+
+if ($file->exists) {
+    my $hatena = WebService::Hatena::Fotolife->new;
+    $hatena->username($username);
+    $hatena->password($upload_key);
+
+    my ($date) =  $hatena->createEntry(
+        title    => $ARGV[0],
+        filename => $file,
+        folder   => $folder,
+    ) =~ /(\d{14})$/;
+    my $url = sprintf(
+        "http://f.st-hatena.com/images/fotolife/%s/%s/%s/%s.png",
+        substr($username, 0, 1),
+        $username,
+        substr($date, 0, 8),
+        $date,
+    );
+
+    run(['echo', $url], '|', [qw(pbcopy)]);
+    print "Successfully uploaded: $url\n";
+}
