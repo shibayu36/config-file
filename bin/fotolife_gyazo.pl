b27726c5527ab6a3887ad4dabd5b6ba804de433b
shibayu36 <shibayu36@gmail.com>
2014-03-19 22:31:05 +0900

    どんな大きさでもfotolifeつかえる

--- bin/fotolife_gyazo.pl
+++ bin/fotolife_gyazo.pl
@@ -4,6 +4,7 @@ use Config::Pit;
 use IPC::Run qw(run);
 use WebService::Hatena::Fotolife;
 use Path::Tiny;
+use LWP::UserAgent;
 
 my $config = pit_get("f.hatena.ne.jp", require => {
     username   => "username",
@@ -28,15 +29,28 @@ if ($file->exists) {
         filename => $file,
         folder   => $folder,
     ) =~ /(\d{14})$/;
-    my $url = sprintf(
-        "http://f.st-hatena.com/images/fotolife/%s/%s/%s/%s_original.png",
+
+    my $foto_url = get_image_url($username, $date);
+
+    run(['echo', $foto_url], '|', [qw(pbcopy)]);
+    system "open $foto_url";
+    print "Successfully uploaded: $foto_url\n";
+}
+
+sub get_image_url {
+    my ($username, $date) = @_;
+
+    my $url_base = sprintf(
+        "http://f.st-hatena.com/images/fotolife/%s/%s/%s/",
         substr($username, 0, 1),
         $username,
         substr($date, 0, 8),
-        $date,
     );
+    my $original_image_url = $url_base . "$date\_original.png";
+    my $image_url = $url_base . "$date.png";
 
-    run(['echo', $url], '|', [qw(pbcopy)]);
-    system "open $url";
-    print "Successfully uploaded: $url\n";
+    my $ua = LWP::UserAgent->new;
+    for my $url ($original_image_url, $image_url) {
+        return $url if $ua->head($url)->is_success;
+    }
 }
