8c3277b0bb29b7d2e728d8b055dbbb9b3decdc53
shibayu36 <shibayu36@gmail.com>
2020-02-05 14:49:57 +0900

    installerにいろいろ追加

--- installer.sh
+++ installer.sh
@@ -32,5 +32,18 @@ brew install yarn
 brew install mysql@5.6
 brew install ctags-exuberant
 brew install tig
+brew install awscli
+brew cask install session-manager-plugin
+
+brew install --HEAD motemen/furoshiki2/furoshiki2
+
+brew install amazon-ecs-cli
+brew install jq
+brew install telnet
+brew install mackerelio/mackerel-agent/mackerel-agent
+brew install mackerelio/mackerel-agent/mkr
+brew install hub
 
 brew cask install adoptopenjdk8
+brew cask install sequel-pro
+brew cask install spotify
