47eec22f90ed0238ec2155cb95ec3cf693bbaccc
shibayu36 <shibayu36@gmail.com>
2021-10-19 11:06:16 +0900

    Add --limit to hub pr list to fetch prs faster

--- .zsh/peco-sources/git-recent-branches.zsh
+++ .zsh/peco-sources/git-recent-branches.zsh
@@ -23,7 +23,7 @@ function peco-git-recent-all-branches () {
 zle -N peco-git-recent-all-branches
 
 function peco-git-recent-pull-requests () {
-    local selected_pr_number=$(hub pr list --sort updated --format "%pC%>(8)%i%Creset  %t (by @%au)%n" | peco | sed -r 's/^ +#([0-9]+).*$/\1/')
+    local selected_pr_number=$(hub pr list --limit 40 --sort updated --format "%pC%>(8)%i%Creset  %t (by @%au)%n" | peco | sed -r 's/^ +#([0-9]+).*$/\1/')
     if [ -n "$selected_pr_number" ]; then
         BUFFER="hub pr checkout ${selected_pr_number}"
         zle accept-line
