3a85386c3801e0ec3fb07349c6d5a616a5a695d0
shibayu36 <shibayu36@gmail.com>
2021-12-11 11:54:06 +0900

    Use gh command instead of hub to checkout recent pull requests

--- .zsh/peco-sources/git-recent-branches.zsh
+++ .zsh/peco-sources/git-recent-branches.zsh
@@ -23,9 +23,9 @@ function peco-git-recent-all-branches () {
 zle -N peco-git-recent-all-branches
 
 function peco-git-recent-pull-requests () {
-    local selected_pr_number=$(hub pr list --limit 40 --sort updated --format "%pC%>(8)%i%Creset  %t (by @%au)%n" | peco | sed -r 's/^ +#([0-9]+).*$/\1/')
+    local selected_pr_number=$(gh pr list --limit 50 --search 'sort:updated-desc' | peco | awk '{ print $1 }')
     if [ -n "$selected_pr_number" ]; then
-        BUFFER="hub pr checkout ${selected_pr_number}"
+        BUFFER="gh pr checkout ${selected_pr_number}"
         zle accept-line
     fi
     zle clear-screen
