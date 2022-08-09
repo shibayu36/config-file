6869ca01199db264f894bdf40f24c33f72253d75
shibayu36 <shibayu36@gmail.com>
2022-08-10 00:14:30 +0900

    Fix installer.sh

--- installer.sh
+++ installer.sh
@@ -19,11 +19,14 @@ brew install shared-mime-info
 brew install libjpeg
 brew install little-cms2
 
+brew install --cask raycast
+
 # for zshrc
 brew install zplug
 brew install tmux
 brew install git-secrets
 brew install peco
+brew install ghq
 brew install asdf
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
@@ -46,9 +49,9 @@ brew install mycli
 
 brew install amazon-ecs-cli
 brew install telnet
-brew install mackerelio/mackerel-agent/mackerel-agent
-brew install mackerelio/mackerel-agent/mkr
-brew install hub
+# brew install mackerelio/mackerel-agent/mackerel-agent
+# brew install mackerelio/mackerel-agent/mkr
+# brew install hub
 brew install prettier
 brew install tree
 brew install gibo
@@ -56,19 +59,18 @@ brew install colordiff
 brew install aws/tap/aws-sam-cli
 brew install awslogs
 brew install terraform
-brew install scalacenter/bloop/bloop
+# brew install scalacenter/bloop/bloop
 brew install weaveworks/tap/eksctl
 brew install aws-iam-authenticator
 brew install graphviz
-brew install itchyny/tap/gojq
-brew install github/gh/gh
+brew install gojq
+brew install gh
 brew install envchain
-brew install Songmu/tap/blogsync
-brew install git-subrepo
+# brew install Songmu/tap/blogsync
+# brew install git-subrepo
 brew install htop
 brew install heroku/brew/heroku
 brew install wget
-brew install ghq
 brew install itchyny/tap/fillin
 brew install mkcert
 brew install findutils
@@ -81,25 +83,26 @@ brew install --cask aws-vault
 
 brew install --cask session-manager-plugin
 brew install --cask wireshark
-brew install --cask adoptopenjdk8
-brew install --cask sequel-pro
+# brew install --cask adoptopenjdk8
+# brew install --cask sequel-pro
 brew install --cask spotify
 brew install --cask jasper
-brew install --cask night-owl
+# brew install --cask night-owl
+brew install --cask docker
+# brew install --cask clipy
+brew install --cask istat-menus
+brew install --cask authy
+brew install --cask bartender
 brew install --cask postman
 brew install --cask tableplus
 brew install --cask steam
-brew install --cask clipy
 brew install --cask asana
-brew install --cask istat-menus
 brew install --cask realforce
-brew install --cask docker
 brew install --cask krisp
-brew install --cask authy
 
 # mysqlenv
-curl -kL http://bit.ly/mysqlenv | bash
+# curl -kL http://bit.ly/mysqlenv | bash
 
 # diff-highlight
 # https://udomomo.hatenablog.com/entry/2019/12/01/181404
-sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+# sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
