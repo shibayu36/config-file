55b2c49d14e9c117b7cac99a8236452031397ab3
shibayu36 <shibayu36@gmail.com>
2022-08-09 22:18:47 +0900

    Remove needless code

--- auto-config.sh
+++ auto-config.sh
@@ -25,7 +25,6 @@ ln -s ~/development/config-file/.hammerspoon ~/
 ln -s ~/development/config-file/karabiner ~/.config/
 ln -s ~/development/config-file/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/development/config-file/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -s ~/development/config-file/vscode/ ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/development/config-file/vscode/snippets ~/Library/Application\ Support/Code/User/
 ln -s ~/development/config-file/.vscode-powertools ~/
 ln -s ~/development/config-file/.textlintrc ~/
