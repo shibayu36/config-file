771ac5cb24de42899e2a68176320bf86344ea74b
shibayu36 <shibayu36@gmail.com>
2023-06-14 12:38:28 +0900

    Remove unnecessary

--- installer.sh
+++ installer.sh
@@ -38,19 +38,19 @@ asdf plugin-add python
 
 # brew install direnv
 brew install keychain
-brew install memcached
-brew install redis
+# brew install memcached
+# brew install redis
 brew install go
 brew install cmigemo
 brew install sshuttle
 brew install yarn
-brew install mysql@5.7
+# brew install mysql@5.7
 brew install universal-ctags
 brew install tig
 brew install awscli
-brew install mycli
+# brew install mycli
 
-brew install amazon-ecs-cli
+# brew install amazon-ecs-cli
 brew install telnet
 # brew install mackerelio/mackerel-agent/mackerel-agent
 # brew install mackerelio/mackerel-agent/mkr
@@ -60,11 +60,11 @@ brew install tree
 brew install gibo
 brew install colordiff
 brew install aws/tap/aws-sam-cli
-brew install awslogs
+# brew install awslogs
 brew install terraform
 # brew install scalacenter/bloop/bloop
-brew install weaveworks/tap/eksctl
-brew install aws-iam-authenticator
+# brew install weaveworks/tap/eksctl
+# brew install aws-iam-authenticator
 brew install graphviz
 brew install gojq
 brew install gh
@@ -72,16 +72,16 @@ brew install envchain
 # brew install Songmu/tap/blogsync
 # brew install git-subrepo
 brew install htop
-brew install heroku/brew/heroku
+# brew install heroku/brew/heroku
 brew install wget
 brew install itchyny/tap/fillin
 brew install mkcert
 brew install findutils
 
-brew install helm
-helm plugin install https://github.com/jkroepke/helm-secrets
+# brew install helm
+# helm plugin install https://github.com/jkroepke/helm-secrets
 brew install sops
-brew install yq
+# brew install yq
 
 brew install golangci-lint
 
@@ -91,19 +91,19 @@ brew install --cask wireshark
 # brew install --cask adoptopenjdk8
 # brew install --cask sequel-pro
 brew install --cask spotify
-brew install --cask jasper
+# brew install --cask jasper
 # brew install --cask night-owl
 brew install --cask docker
 # brew install --cask clipy
 brew install --cask istat-menus
 brew install --cask authy
 brew install --cask bartender
-brew install --cask postman
+# brew install --cask postman
 brew install --cask tableplus
 brew install --cask steam
 brew install --cask asana
 brew install --cask realforce
-brew install --cask krisp
+# brew install --cask krisp
 
 # mysqlenv
 # curl -kL http://bit.ly/mysqlenv | bash
