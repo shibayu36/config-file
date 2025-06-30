c16a5888388f48d10e97410b99de13dddee0195e
shibayu36 <shibayu36@gmail.com>
2025-07-01 08:59:17 +0900

    installer.shの整理整頓

--- installer.sh
+++ installer.sh
@@ -39,41 +39,26 @@ asdf plugin-add python
 
 brew install direnv
 brew install keychain
-# brew install memcached
-# brew install redis
 brew install go
 brew install cmigemo
 brew install sshuttle
-brew install yarn
-# brew install mysql@5.7
 brew install universal-ctags
 brew install tig
 brew install awscli
-# brew install mycli
+brew install mycli
 
-# brew install amazon-ecs-cli
 brew install telnet
-# brew install mackerelio/mackerel-agent/mackerel-agent
-# brew install mackerelio/mackerel-agent/mkr
-# brew install hub
 brew install prettier
 brew install tree
 brew install gibo
 brew install colordiff
-brew install aws/tap/aws-sam-cli
-# brew install awslogs
 brew install terraform
-# brew install scalacenter/bloop/bloop
-# brew install weaveworks/tap/eksctl
-# brew install aws-iam-authenticator
 brew install graphviz
 brew install gojq
 brew install gh
 brew install envchain
-# brew install Songmu/tap/blogsync
-# brew install git-subrepo
+brew install Songmu/tap/blogsync
 brew install htop
-# brew install heroku/brew/heroku
 brew install wget
 brew install itchyny/tap/fillin
 brew install mkcert
@@ -84,10 +69,7 @@ brew install starship
 brew install jordanbaird-ice
 brew install uv
 
-# brew install helm
-# helm plugin install https://github.com/jkroepke/helm-secrets
 brew install sops
-# brew install yq
 
 brew install golangci-lint
 
@@ -96,17 +78,10 @@ brew install --cask obsidian
 brew install --cask aws-vault
 brew install --cask session-manager-plugin
 brew install --cask wireshark
-# brew install --cask adoptopenjdk8
-# brew install --cask sequel-pro
 brew install --cask spotify
-# brew install --cask jasper
-# brew install --cask night-owl
 brew install --cask docker
-# brew install --cask clipy
 brew install --cask istat-menus
 brew install --cask authy
-# brew install --cask postman
-brew install --cask tableplus
 brew install --cask steam
 brew install --cask asana
 brew install --cask realforce
@@ -115,11 +90,3 @@ brew install --cask discord
 brew install --cask homebrew/cask-fonts/font-hack-nerd-font
 brew install --cask git-credential-manager
 brew install --cask claude
-# brew install --cask krisp
-
-# mysqlenv
-# curl -kL http://bit.ly/mysqlenv | bash
-
-# diff-highlight
-# https://udomomo.hatenablog.com/entry/2019/12/01/181404
-# sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
