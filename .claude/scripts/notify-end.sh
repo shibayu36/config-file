5d7f9d58706fc067f6d71b1c663195bf80a362f1
shibayu36 <shibayu36@gmail.com>
2025-10-05 15:39:56 +0900

    contentImage使う

--- .claude/scripts/notify-end.sh
+++ .claude/scripts/notify-end.sh
@@ -25,4 +25,4 @@ else
     MSG="Task completed"
 fi
 
-terminal-notifier -title 'Claude Code' -message "$MSG" -sound Glass -appIcon 'https://cdn.prod.website-files.com/6889473510b50328dbb70ae6/68c33859cc6cd903686c66a2_apple-touch-icon.png'
+terminal-notifier -title 'Claude Code' -message "$MSG" -sound Glass -contentImage 'https://cdn.prod.website-files.com/6889473510b50328dbb70ae6/68c33859cc6cd903686c66a2_apple-touch-icon.png'
