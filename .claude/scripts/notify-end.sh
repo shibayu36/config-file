58995675a416c961bf359a9419003cb21885483d
shibayu36 <shibayu36@gmail.com>
2025-10-28 09:00:02 +0900

    まずはiTerm2を開くように

--- .claude/scripts/notify-end.sh
+++ .claude/scripts/notify-end.sh
@@ -25,4 +25,8 @@ else
     MSG="Task completed"
 fi
 
-terminal-notifier -title 'Claude Code' -message "$MSG" -sound Glass -contentImage 'https://cdn.prod.website-files.com/6889473510b50328dbb70ae6/68c33859cc6cd903686c66a2_apple-touch-icon.png'
+terminal-notifier -title 'Claude Code' \
+    -message "$MSG" \
+    -sound Glass \
+    -contentImage 'https://cdn.prod.website-files.com/6889473510b50328dbb70ae6/68c33859cc6cd903686c66a2_apple-touch-icon.png' \
+    -activate "com.googlecode.iterm2"
