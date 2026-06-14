bd93a72b5788dd9f847d7cd76f6ada06a86426fc
shibayu36 <shibayu36@gmail.com>
2026-06-15 05:52:33 +0900

    ctrl+/のNSBeepを抑止するDefaultKeyBinding.dictを追加

--- auto-config.sh
+++ auto-config.sh
@@ -22,6 +22,7 @@ ln -s ~/development/config-file/.cheat ~/.config/cheat/cheatsheets/personal
 ln -s ~/development/config-file/.tigrc ~/
 ln -s ~/development/config-file/.hammerspoon ~/
 ln -s ~/development/config-file/karabiner ~/.config/
+ln -s ~/development/config-file/KeyBindings ~/Library/KeyBindings
 ln -s ~/development/config-file/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/development/config-file/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/development/config-file/vscode/snippets ~/Library/Application\ Support/Code/User/
