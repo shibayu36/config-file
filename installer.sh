6abdf37b6a61a17d960c5ad525b6312c1c36effa
shibayu36 <shibayu36@gmail.com>
2026-09-10 20:32:28 +0900

    installer.shをBrewfileベースに置き換える
    brew installの羅列をやめ、新しいMacでも必ず入れたいものだけを
    Brewfileに書く。installer.shはHomebrewの導入・brew bundle install・
    asdf pluginの追加だけを行う。更新は brew upgrade で意図的に行うため、
    brew bundle install には --no-upgrade を付ける。
    
    Brewfileには現在のMacで使っているものだけを載せ、以下は載せない。
    - asdfで管理しているランタイム（node / ruby / python / yarn など）
    - 長期間使っていないツールや、依存として入るライブラリ
    - 使わなくなったもの（mycli、ctags、git-credential-manager、
      Docker Desktop など）
    
    Docker DesktopはOrbStackに置き換える。

--- installer.sh
+++ installer.sh
@@ -2,97 +2,23 @@
 set -eu
 
 # homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew >/dev/null 2>&1; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# development fundamental
-brew install coreutils
-brew install gnu-sed
-brew install gawk
-brew install autoconf
-brew install automake
-brew install pkg-config
-brew install gnutls
-brew install texinfo
-brew install gettext
-brew install git
-brew install git-delta
-brew install libxml2
-brew install openssl
-brew install shared-mime-info
-brew install libjpeg
-brew install little-cms2
-brew install pam-reattach
+# 更新は brew upgrade で意図的に行うため、ここでは不足分のインストールだけを行う
+brew bundle install --no-upgrade --file="$(cd "$(dirname "$0")" && pwd)/Brewfile"
 
-brew install --cask font-hack-nerd-font
-
-brew install --cask raycast
-brew install --cask karabiner-elements
-brew install --cask ghostty
-
-# for zshrc
-brew install tmux
-brew install herdr
-brew install fzf
-brew install bat
-brew install ripgrep
-brew install ghq
-brew install asdf
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf plugin add perl
-asdf plugin-add python
-
-brew install direnv
-brew install keychain
-brew install go
-brew install cmigemo
-brew install sshuttle
-brew install universal-ctags
-brew install tig
-brew install awscli
-brew install mycli
-
-brew install telnet
-brew install prettier
-brew install tree
-brew install gibo
-brew install colordiff
-brew install tfenv
-brew install graphviz
-brew install gojq
-brew install gh
-brew install envchain
-brew install Songmu/tap/blogsync
-brew install htop
-brew install wget
-brew install itchyny/tap/fillin
-brew install mkcert
-brew install findutils
-brew install protobuf
-brew install git-lfs
-brew install starship
-brew install deck
-brew install uv
-brew install vjeantet/tap/alerter
-
-brew install sops
-
-brew install golangci-lint
-
-brew install --cask jordanbaird-ice
-brew install --cask 1password-cli
-
-brew install --cask obsidian
-brew install --cask aws-vault
-brew install --cask session-manager-plugin
-brew install --cask wireshark
-brew install --cask spotify
-brew install --cask docker
-brew install --cask istat-menus
-brew install --cask steam
-brew install --cask asana
-brew install --cask realforce
-brew install --cask deepl
-brew install --cask discord
-brew install --cask git-credential-manager
-brew install --cask claude
+# asdf plugins
+asdf_plugin_add() {
+  local name=$1
+  shift
+  if asdf plugin list | grep -qx "$name"; then
+    return
+  fi
+  asdf plugin add "$name" "$@"
+}
+asdf_plugin_add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf_plugin_add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf_plugin_add perl
+asdf_plugin_add python
