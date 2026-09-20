1b726d91bb832800bd1e1719305f0d9cc8e41de1
shibayu36 <shibayu36@gmail.com>
2026-09-20 14:54:27 +0900

    installer.shでrustupを入れるようにする
    toolchainの切り替えやcomponent追加でrustupを使うため、他の言語と違い
    asdfではなくrustupで管理する。公式インストーラがstable toolchainまで
    入れるので、これだけでcargoが使える。

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Brewfileの内容とasdfのpluginを入れる。Homebrew自体はbootstrap.shで入れる
+# Brewfileの内容とasdfのplugin、Rustを入れる。Homebrew自体はbootstrap.shで入れる
 set -eu
 
 # 更新は brew upgrade で意図的に行うため、ここでは不足分のインストールだけを行う
@@ -18,3 +18,9 @@ asdf_plugin_add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf_plugin_add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf_plugin_add perl
 asdf_plugin_add python
+
+# Rust。toolchainはasdfではなくrustupで管理する
+# --no-modify-pathはPATHを.zshenvで管理しているため。付けないとrustupが~/.zshenvを書き換える
+if [ ! -x "$HOME/.cargo/bin/rustup" ]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+fi
